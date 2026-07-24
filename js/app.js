83589f53829808f4b3478272e6d7ba3bc8ec6ee1
MahendranDM <mahendrandm2003@gmail.com>
2026-07-24 11:14:19 +0530

    Add dynamic movie catalogue page

--- js/app.js
+++ js/app.js
@@ -32,4 +32,61 @@ app.controller("bookingController", function ($scope) {
 
     ];
 
+
+    $scope.movies = [
+
+{
+name:"Leo",
+genre:"Action",
+language:"Tamil",
+duration:"2h 45m",
+show:"10:00 AM",
+price:250
+},
+
+{
+name:"Avengers Endgame",
+genre:"Action",
+language:"English",
+duration:"3h",
+show:"1:30 PM",
+price:350
+},
+
+{
+
+name:"Premalu",
+genre:"Romance",
+language:"Malayalam",
+duration:"2h 20m",
+show:"4:00 PM",
+price:200
+},
+
+{
+name:"KGF Chapter 2",
+genre:"Action",
+language:"Kannada",
+duration:"2h 50m",
+show:"7:00 PM",
+price:300
+},
+
+{
+
+name:"Interstellar",
+genre:"Sci-Fi",
+language:"English",
+duration:"2h 49m",
+show:"9:30 PM",
+price:400
+}
+
+];
+
+
+
+
+
+
 });
